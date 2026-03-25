d0054ad7c0232edc4dae10160cb8e2c1ff3fd668
mudinyver <mael.udin--yver@etudiant.univ-rennes.fr>
2026-03-25 11:59:52 +0100

    réparation Type

--- html/test/Type.test.js
+++ html/test/Type.test.js
@@ -1,5 +1,6 @@
 import * as truc from './import.test.js';
 
-truc.Type.fillTypes(truc.type_effectiveness['type_effectiveness']);
-// console.log(truc.Type.all_types[1].type_name);
+console.log(truc.type_effectiveness);
+console.log(truc.Type.fillTypes(truc.type_effectiveness));
+// console.log(truc.Type.all_types);
 console.log(truc.Type.all_types[0].toString());
